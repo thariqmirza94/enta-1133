7af1908eb4488bb7a6b51d97005185fd36676fee
thariqmirza94 <thariqmirza94@gmail.com>
2024-10-04 18:23:02 -0700

    GD12_1133_A1_ThariqMirza
    Assignment 1

---
+++ GD12_1133_Lab1_Mirza_Thariq/DieGame/DieGame/DieGame/Scripts/Pill.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieGame.Scripts
+{
+    public class Pill
+    {
+        private Random random = new Random();
+        private int[] pillLevels = { 0, 10, 20, 30, 50 };
+        public int ComputerPoisonLevel { get; set; } = 0;
+
+        public int GetRandomPill()
+        {
+            return pillLevels[random.Next(pillLevels.Length)];
+        }
+    }
+}
